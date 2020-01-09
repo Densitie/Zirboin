766c8a5f60900f8e1f8924f0ed8df871b1bd4373
Densitie <55253212+Densitie@users.noreply.github.com>
2020-01-08 20:47:21 -0500

    Update Zirboin.java

--- src/Zirboin.java
+++ src/Zirboin.java
@@ -1,12 +1,12 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Arun k
+ * This is the planet Zirboin currency converter program
+ * Dec 1 2019
  */
 
 /**
  *
- * @author ajayk
+ * @author arkir7294
  */
 public class Zirboin extends javax.swing.JFrame {
     double amountTotal;
